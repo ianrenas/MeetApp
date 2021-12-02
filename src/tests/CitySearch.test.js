a4b2320545852b10eef12d99653b69ec85fac942
ianrenas <85459271+ianrenas@users.noreply.github.com>
2021-12-02 01:16:59 -0500

    Main

--- src/tests/CitySearch.test.js
+++ src/tests/CitySearch.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import CitySearch from "../CitySearch";
 import { mockData } from "../mock-data";
-import { extractLocations } from '../api';
+import { extractLocations } from "../api";
 
 describe('<CitySearch /> component', () => {
   let locations, CitySearchWrapper;
