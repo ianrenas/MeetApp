4ceb3460a357f0055088639fefed5231369a7b3b
ianrenas <85459271+ianrenas@users.noreply.github.com>
2021-12-13 23:06:09 -0500

    12.13.04

--- src/api.js
+++ src/api.js
@@ -45,7 +45,7 @@ export const getEvents = async () => {
 
   if (token) {
     removeQuery();
-    const url = 'https://fo3jdyed01.execute-api.us-east-1.amazonaws.com/dev/api/get-events/' + '/' + token;
+    const url = 'https://fo3jdyed01.execute-api.us-east-1.amazonaws.com/dev/api/get-events' + '/' + token;
     const result = await axios.get(url);
     if (result.data) {
       var locations = extractLocations(result.data.events);
