9b3ca7e80f43b3aa01e5a343bf6fa3491d211e2a
ianrenas <85459271+ianrenas@users.noreply.github.com>
2021-12-18 18:11:13 -0500

    12.18.00

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+
+atatus.config('24cd2d25b98c4ceab041c9fd36352dff').install();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,3 +24,4 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
