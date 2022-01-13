0a404f0c03142257aa001a05ca5ee87ece630c71
ianrenas <85459271+ianrenas@users.noreply.github.com>
2022-01-13 17:12:36 -0500

    01.13.00

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import './nprogress.css';
 import './App.css';
 import { InfoAlert } from './Alert';
 import WelcomeScreen from './WelcomeScreen';
+import {
+  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip
+} from 'recharts';
 
 
 
@@ -57,14 +60,23 @@ class App extends Component {
     this.updateEvents(currentLocation, eventCount);
   }
 
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return { city, number };
+    })
+    return data;
+  };
+
   componentWillUnmount() {
     this.mounted = false;
   }
 
   render() {
-    if (this.state.showWelcomeScreen === undefined) {
-      return <div className="App" />
-    }
+    if (this.state.showWelcomeScreen === undefined) return <div className="App" />
+
     return (
       <div className="App">
         {!navigator.onLine ? (<InfoAlert text='You are offline!' />) : (<InfoAlert text=' ' />)}
@@ -72,6 +84,20 @@ class App extends Component {
         <NumberOfEvents
           numberOfEvents={this.state.numberOfEvents}
           updateNumberOfEvents={this.updateNumberOfEvents} />
+
+        <ScatterChart
+          width={400}
+          height={400}
+          margin={{
+            top: 20, right: 20, bottom: 20, left: 20,
+          }}>
+          <CartesianGrid />
+          <XAxis type="number" dataKey="x" name="stature" unit="cm" />
+          <YAxis type="number" dataKey="y" name="weight" unit="kg" />
+          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+          <Scatter data={this.getData()} fill="#8884d8" />
+        </ScatterChart>
+
         <EventList events={this.state.events} />
         <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
           getAccessToken={() => { getAccessToken() }} />
